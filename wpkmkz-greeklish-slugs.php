b8c5903d69f10932d4a4ca2d0793e9ae7504deba
Kostas Skapator Charalampidis <skapator@gmail.com>
2013-08-04 19:57:41 +0300

    release v1

--- wpkmkz-greeklish-slugs.php
+++ wpkmkz-greeklish-slugs.php
@@ -4,7 +4,7 @@
     Plugin URI: http://www.wpkamikaze.com
     Description: Translates greek character slugs to latin (greeklish) for posts, terms, files.
     Author: Kostas Skapator Charal
-    Version: 1.1
+    Version: 1.0
     Author URI: http://www.skapator.com/
     License: GPL
     Copyright: Kostas Skapator Charal
